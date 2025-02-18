74bec901b3945a193f07ca90a0af6b51a6d9faa1
DamianMorganti14 <144267354+DamianMorganti14@users.noreply.github.com>
2025-02-18 13:50:03 -0400

    added page and input

---
+++ src/app/damian/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+
+import Input from '@/mk/components/forms/Input/Input'
+import React, { useState } from 'react'
+
+const page = () => {
+    const [name,setName]= useState('')
+  return (
+    <div>
+
+        <Input 
+        name={'sas'}
+         type="text"
+         label='asa'
+        // placeholder="Enter your name"
+         value={name}
+         onChange={(e) => setName(e.target.value)}
+
+
+        />
+    </div>
+  )
+}
+
+export default page
