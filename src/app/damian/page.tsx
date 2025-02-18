966fe2301e00836ec1d70be33b76be9c9ccd647c
DamianMorganti14 <144267354+DamianMorganti14@users.noreply.github.com>
2025-02-18 15:35:19 -0400

    added button color

--- src/app/damian/page.tsx
+++ src/app/damian/page.tsx
@@ -1,13 +1,17 @@
 'use client'
 
 
+import Button from '@/mk/components/forms/Button/Button'
 import Input from '@/mk/components/forms/Input/Input'
 import TextArea from '@/mk/components/forms/TextArea/TextArea'
+import DataModal from '@/mk/components/ui/DataModal/DataModal'
 import React, { useState } from 'react'
 
 const page = () => {
-    const [name,setName]= useState('')
+    const [name,setName]= useState('');
+    const [open,setOpen]=useState(false)
   return (
+    <>
     <div>
 
         <Input 
@@ -28,7 +32,28 @@ const page = () => {
         value={name}
         onChange={(e) => setName(e.target.value)}
         />
+        <Button onClick={() => setOpen(true)}>Open Modal</Button>
     </div>
+    <DataModal
+      open={open}
+      onClose={() => setOpen(false)}
+      title="Modal title"
+    
+      >aaa
+      
+      
+      <Input 
+        name={'sas'}
+         type="text"
+         label='asa'
+        // placeholder="Enter your name"
+         value={name}
+         onChange={(e) => setName(e.target.value)}
+
+
+        />
+</DataModal>
+    </>
   )
 }
 
