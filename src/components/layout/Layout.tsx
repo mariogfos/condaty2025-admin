e76a7285b2557134996a20e972e905bcb0bbb6b5
cristian <crisblanco.fos@gmail.com>
2025-04-28 10:25:55 -0400

    feat(layout): add alert system with sound and modal
    Introduce a new alert system in the Layout component that triggers a sound and displays a modal when an alert event is received. The system includes:
    - A sound file (`Alerta.mp3`) played when an alert is triggered.
    - A modal displaying alert details, including resident information and alert type.
    - Icons for different alert types (medical, emergency, theft).
    - Audio enabled after user interaction to comply with browser autoplay policies.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -12,8 +12,29 @@ import { useRouter } from "next/navigation";
 import { getFormattedDate } from "@/mk/utils/date";
 import SideMenu from "@/mk/components/ui/SideMenu/SideMenu";
 import { useEvent } from "@/mk/hooks/useEvents";
+import ItemList from "@/mk/components/ui/ItemList/ItemList";
+import { Avatar } from "@/mk/components/ui/Avatar/Avatar";
+import { getUrlImages } from "@/mk/utils/string";
+import { IconAmbulance, IconFlame, IconTheft } from "./icons/IconsBiblioteca";
 
 // const soundBell = new Audio("/sounds/bellding.mp3");
+const typeAlerts: any = {
+  E: {
+    name: "Emergencia Medica",
+    icon: <IconAmbulance size={36} color="var(--cWhite)" />,
+    color: { background: "var(--cHoverError)", border: "var(--cError)" },
+  },
+  F: {
+    name: "Emergencia",
+    icon: <IconFlame size={36} color="var(--cWhite)" />,
+    color: { background: "var(--cHoverWarning)", border: "var(--cWarning)" },
+  },
+  T: {
+    name: "Alerta",
+    icon: <IconTheft size={36} color="var(--cWhite)" />,
+    color: { background: "var(--cHoverInfo)", border: "var(--cInfo)" },
+  },
+};
 
 const Layout = ({ children }: any) => {
   const { user, logout, store, showToast } = useAuth();
@@ -22,8 +43,13 @@ const Layout = ({ children }: any) => {
   const [sideMenuOpen, setSideMenuOpen] = useState(false);
   const [client, setClient]: any = useState(null);
   const [onLogout, setOnLogout] = useState(false);
+  const [openAlert, setOpenAlert]: any = useState({ open: false, item: null });
   const path: any = usePathname();
   const router = useRouter();
+  const [audioEnabled, setAudioEnabled] = useState(false);
+  const [soundBell] = useState(
+    typeof window !== "undefined" ? new Audio("/sounds/Alerta.mp3") : null
+  );
 
   useEffect(() => {
     if (user) {
@@ -32,21 +58,57 @@ const Layout = ({ children }: any) => {
     }
   }, [user]);
 
-  const onNotif = useCallback((e: any) => {
-    console.log("*******11111*****", e);
-    if (e.event == "ping") {
-      showToast("Llegó un PING", "info");
-    }
+  useEffect(() => {
+    // Habilitar el audio después de la primera interacción del usuario
+    const enableAudio = () => {
+      soundBell
+        ?.play()
+        .then(() => {
+          soundBell?.pause();
+          soundBell?.load();
+          setAudioEnabled(true);
+          // Remover el evento después de la primera interacción
+          document.removeEventListener("click", enableAudio);
+        })
+        .catch((error) => console.log("Error al habilitar el audio:", error));
+    };
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    document.addEventListener("click", enableAudio);
+    return () => {
+      document.removeEventListener("click", enableAudio);
+    };
+  }, [soundBell]);
+
+  const onNotif = useCallback(
+    (e: any) => {
+      console.log("*******11111*****", e);
+      if (e.event == "ping") {
+        showToast("Llegó un PING", "info");
+      }
+      if (e.event == "alerts" && e.payload?.level == 4) {
+        setOpenAlert({ open: true, item: e.payload });
+        if (audioEnabled) {
+          soundBell
+            ?.play()
+            .catch((error) =>
+              console.log("Error al reproducir el sonido:", error)
+            );
+        }
+      }
+    },
+    [soundBell, showToast, audioEnabled]
+  );
 
   useEvent("onNotif", onNotif);
 
   const layoutClassName = `${styles.layout} ${
     isDesktop && !sideMenuOpen ? styles.layoutExpanded : ""
   } ${isDesktop && sideMenuOpen ? styles.layoutCollapsed : ""}`;
-
+  const onCloseAlert = () => {
+    setOpenAlert({ open: false, item: null });
+    soundBell?.pause();
+    soundBell?.load();
+  };
   return (
     <main className={layoutClassName}>
       <section>
@@ -105,6 +167,51 @@ const Layout = ({ children }: any) => {
         <p className={styles.modalLogout}>
           ¿Estás seguro de que deseas cerrar sesión?
         </p>
+      </DataModal>
+
+      <DataModal
+        style={{ border: "1px solid var(--cError)" }}
+        title="Nueva emergencia"
+        iconClose={false}
+        open={openAlert.open}
+        buttonCancel=""
+        buttonText="Atender"
+        onClose={onCloseAlert}
+        onSave={onCloseAlert}
+      >
+        <p style={{ color: "var(--cWhiteV1)", marginBottom: 8 }}>Residente</p>
+        <ItemList
+          variant="V1"
+          title={openAlert?.item?.owner_name}
+          subtitle={"Unidad: " + openAlert?.item?.unit}
+          left={
+            <Avatar
+              src={getUrlImages(
+                "/OWNER-" + openAlert?.item?.owner_id + ".webp?d="
+              )}
+            />
+          }
+        />
+        <p style={{ color: "var(--cWhiteV1)", marginBottom: 8 }}>
+          Tipo de emergencia
+        </p>
+        <div
+          style={{
+            backgroundColor:
+              typeAlerts[openAlert?.item?.type]?.color?.background,
+            borderRadius: 8,
+            border: `1px solid ${
+              typeAlerts[openAlert?.item?.type]?.color?.border
+            }`,
+            width: "184px",
+            padding: 8,
+            margin: "0 auto",
+            color: "var(--cWhite)",
+          }}
+        >
+          {typeAlerts[openAlert?.item?.type]?.icon}
+          <p>{openAlert?.item?.name}</p>
+        </div>
       </DataModal>
     </main>
   );
