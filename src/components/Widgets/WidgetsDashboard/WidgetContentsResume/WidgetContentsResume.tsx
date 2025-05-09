8a68a1cb93a41bb36288da8de234b8de75cf5487
Damian <damianmorganti14@gmail.com>
2025-05-09 09:57:49 -0400

    style: adjust widget layout and height for better responsiveness
    Update CSS to use flexbox for widget contents and modify maxHeight in WidgetContentsResume to use percentage for better responsiveness

--- src/components/Widgets/WidgetsDashboard/WidgetContentsResume/WidgetContentsResume.tsx
+++ src/components/Widgets/WidgetsDashboard/WidgetContentsResume/WidgetContentsResume.tsx
@@ -179,7 +179,7 @@ import { IconComment, IconLike } from '@/components/layout/icons/IconsBiblioteca
 const WidgetContentsResume = ({data}:any) => {
   return (
     <WidgetBase variant={'V1'} title={'Comunidad'} >
-    <div style={{maxHeight: '600px', overflowY: 'auto'}}>
+    <div style={{maxHeight: '90%', overflowY: 'auto'}}>
     {data && data.map((item: any, index: number) => (
       <div key={item.id}>
       <div className='bottomLine'/>
