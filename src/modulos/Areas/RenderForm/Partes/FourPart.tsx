a6eb25535a1cd6ad4970ec2c54957dcb8f44ec80
cristian <crisblanco.fos@gmail.com>
2025-07-07 14:38:36 -0400

    refactor(RenderForm): restructure FourPart component and add DataModal for policies

--- src/modulos/Areas/RenderForm/Partes/FourPart.tsx
+++ src/modulos/Areas/RenderForm/Partes/FourPart.tsx
@@ -1,4 +1,3 @@
-import { Card } from "@/mk/components/ui/Card/Card";
 import React, { useState } from "react";
 import styles from "../../RenderView/RenderView.module.css";
 import {
@@ -11,6 +10,7 @@ import {
 import KeyValue from "@/mk/components/ui/KeyValue/KeyValue";
 import { formatNumber } from "@/mk/utils/numbers";
 import { getUrlImages } from "@/mk/utils/string";
+import DataModal from "@/mk/components/ui/DataModal/DataModal";
 const status: any = {
   A: "Activa",
   X: "Inactiva",
@@ -57,197 +57,211 @@ const FourPart = ({ item }: { item: any }) => {
     );
   };
   return (
-    <div className={styles.renderView}>
-      <div className={styles.containerFirstSection}>
-        <div className={styles.containerImage}>
-          <div className={styles.image}>
-            {item?.images?.[indexVisible]?.id && (
-              <img
-                alt=""
-                width={"100%"}
-                height={"auto"}
-                src={getUrlImages(
-                  "/AREA-" +
-                    item?.id +
-                    "-" +
-                    item?.images?.[indexVisible]?.id +
-                    ".webp" +
-                    "?" +
-                    item?.updated_at
-                )}
-              />
+    <>
+      <div className={styles.renderView}>
+        <div className={styles.containerFirstSection}>
+          <div className={styles.containerImage}>
+            <div className={styles.image}>
+              {item?.images?.[indexVisible]?.id && (
+                <img
+                  alt=""
+                  width={"100%"}
+                  height={"auto"}
+                  src={getUrlImages(
+                    "/AREA-" +
+                      item?.id +
+                      "-" +
+                      item?.images?.[indexVisible]?.id +
+                      ".webp" +
+                      "?" +
+                      item?.updated_at
+                  )}
+                />
+              )}
+            </div>
+            {item?.images?.length > 1 && (
+              <div className={styles.containerButton}>
+                <div className={styles.button} onClick={prevIndex}>
+                  <IconArrowLeft size={18} color="var(--cWhite)" />
+                </div>
+                <p style={{ color: "var(--cWhite)", fontSize: 10 }}>
+                  {indexVisible + 1} / {item?.images?.length}
+                </p>
+                <div className={styles.button} onClick={nextIndex}>
+                  <IconArrowRight size={18} color="var(--cWhite)" />
+                </div>
+              </div>
             )}
           </div>
-          {item?.images?.length > 1 && (
-            <div className={styles.containerButton}>
-              <div className={styles.button} onClick={prevIndex}>
-                <IconArrowLeft size={18} color="var(--cWhite)" />
-              </div>
-              <p style={{ color: "var(--cWhite)", fontSize: 10 }}>
-                {indexVisible + 1} / {item?.images?.length}
-              </p>
-              <div className={styles.button} onClick={nextIndex}>
-                <IconArrowRight size={18} color="var(--cWhite)" />
-              </div>
-            </div>
-          )}
-        </div>
-        <div className={styles.containerInfo}>
-          <p className={styles.title}>{item?.title}</p>
-          <p
-            className={isExpanded ? undefined : styles.truncatedText}
-            style={{ color: "var(--cWhiteV1)" }}
-          >
-            {item?.description}
-          </p>
-          <p
-            style={{
-              color: "var(--cAccent)",
-              cursor: "pointer",
-              width: 100,
-              fontWeight: 600,
-            }}
-            onClick={toggleExpanded}
-          >
-            {isExpanded ? "Ver menos" : "Ver más"}
-          </p>
-          <Br />
-          <p className={styles.title}>Datos generales</p>
-          <KeyValue
-            title={"Estado"}
-            value={status[item?.status]}
-            colorValue={
-              item?.status == "A" ? "var(--cSuccess)" : "var(--cError)"
-            }
-          />
-          <KeyValue
-            title={"Tipo de reserva"}
-            value={item?.booking_mode === "hour" ? "Por hora" : "Por día"}
-          />
+          <div className={styles.containerInfo}>
+            <p className={styles.title}>{item?.title}</p>
+            <p
+              className={isExpanded ? undefined : styles.truncatedText}
+              style={{ color: "var(--cWhiteV1)" }}
+            >
+              {item?.description}
+            </p>
+            <p
+              style={{
+                color: "var(--cAccent)",
+                cursor: "pointer",
+                width: 100,
+                fontWeight: 600,
+              }}
+              onClick={toggleExpanded}
+            >
+              {isExpanded ? "Ver menos" : "Ver más"}
+            </p>
+            <Br />
+            <p className={styles.title}>Datos generales</p>
+            <KeyValue
+              title={"Estado"}
+              value={status[item?.status]}
+              colorValue={
+                item?.status == "A" ? "var(--cSuccess)" : "var(--cError)"
+              }
+            />
+            <KeyValue
+              title={"Tipo de reserva"}
+              value={item?.booking_mode === "hour" ? "Por hora" : "Por día"}
+            />
 
-          <KeyValue
-            title={"Costo"}
-            value={"Bs. " + formatNumber(item?.price)}
-          />
-          <KeyValue
-            title={"Cantidad máx. de personas"}
-            value={item?.max_capacity}
-          />
-          {/* <KeyValue
-            title={"Máximo de reservas por semana"}
-            value={item?.max_reservations_per_week + " reservas"}
-          /> */}
-          <KeyValue
-            title={"Restricción por mora"}
-            value={item?.penalty_or_debt_restriction == "A" ? "Sí" : "No"}
-          />
-          <KeyValue
-            title={"Aprobación de administración"}
-            value={item?.requires_approval == "A" ? "Sí" : "No"}
-          />
-          <KeyValue
-            title={"Máximo de reservas por semana"}
-            value={item?.max_reservations_per_week}
-          />
-          <KeyValue
-            title={"Cancelación sin multa"}
-            value={item?.min_cancel_hours + "h"}
-          />
-          <KeyValue
-            title={"Porcentaje por cancelación"}
-            value={formatNumber(item?.penalty_fee, 1) + "%"}
-          />
+            <KeyValue
+              title={"Costo"}
+              value={"Bs. " + formatNumber(item?.price)}
+            />
+            <KeyValue
+              title={"Cantidad máx. de personas"}
+              value={item?.max_capacity}
+            />
+            <KeyValue
+              title={"Restricción por mora"}
+              value={item?.penalty_or_debt_restriction == "A" ? "Sí" : "No"}
+            />
+            <KeyValue
+              title={"Aprobación de administración"}
+              value={item?.requires_approval == "A" ? "Sí" : "No"}
+            />
+            <KeyValue
+              title={"Máximo de reservas por semana"}
+              value={item?.max_reservations_per_week}
+            />
+            <KeyValue
+              title={"Cancelación sin multa"}
+              value={item?.min_cancel_hours + "h"}
+            />
+            <KeyValue
+              title={"Porcentaje por cancelación"}
+              value={formatNumber(item?.penalty_fee, 1) + "%"}
+            />
+          </div>
         </div>
-      </div>
-      <Br />
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          color: "var(--cWhite)",
-          alignItems: "center",
-        }}
-      >
-        <p className={styles.title}>Políticas</p>
-        <IconExpand onClick={() => setOpenPolicy(!openPolicy)} />
-      </div>
-      <Br />
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          color: "var(--cWhite)",
-        }}
-      >
-        <p className={styles.title}>Días y periodos disponibles</p>
-        {!openDays ? (
-          <IconArrowDown onClick={() => setOpenDays(!openDays)} />
-        ) : (
-          <IconArrowUp onClick={() => setOpenDays(!openDays)} />
-        )}
-      </div>
-      {openDays && (
+        <Br />
         <div
           style={{
             display: "flex",
-            flexDirection: "row",
-            overflowX: "scroll",
-            scrollbarColor: "var(--cWhiteV2) var(--cBlackV2)",
-            gap: 12,
-            marginTop: 12,
+            justifyContent: "space-between",
+            color: "var(--cWhite)",
+            alignItems: "center",
           }}
         >
-          {sortedDays().map((day) => (
-            <div
-              key={day}
-              style={{
-                backgroundColor: "var(--cWhiteV2)",
-                padding: 12,
-                borderRadius: 8,
-                border: "0.5px solid var(--cWhiteV1)",
-              }}
-            >
-              <p
-                style={{
-                  color: "var(--cWhite)",
-                  fontSize: 14,
-                  fontWeight: 500,
-                }}
-              >
-                {day}
-              </p>
-              <p style={{ fontSize: 12, marginBottom: 8 }}>
-                Horario disponible
-              </p>
-              <div
-                style={{
-                  display: "flex",
-                  gap: 8,
-                  overflowX: "auto",
-                  width: "100%",
-                  scrollbarWidth: "thin",
-                  scrollbarColor: "var(--cBlackV2) var(--cWhiteV2) ",
-                }}
-              >
-                {item?.available_hours?.[day]?.map((hour: any) => (
-                  <div
-                    key={hour}
-                    style={{
-                      padding: 8,
-                      borderRadius: 8,
-                      border: "0.5px solid var(--cWhiteV1)",
-                      flex: "0 0 auto",
-                    }}
-                  >
-                    <p style={{ fontSize: 12 }}>{hour}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
+          <p className={styles.title}>Políticas</p>
+          <IconExpand onClick={() => setOpenPolicy(!openPolicy)} />
         </div>
+        <Br />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            color: "var(--cWhite)",
+          }}
+        >
+          <p className={styles.title}>Días y periodos disponibles</p>
+          {!openDays ? (
+            <IconArrowDown onClick={() => setOpenDays(!openDays)} />
+          ) : (
+            <IconArrowUp onClick={() => setOpenDays(!openDays)} />
+          )}
+        </div>
+        {openDays && (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              overflowX: "scroll",
+              scrollbarColor: "var(--cWhiteV2) var(--cBlackV2)",
+              gap: 12,
+              marginTop: 12,
+            }}
+          >
+            {sortedDays().map((day) => (
+              <div
+                key={day}
+                style={{
+                  backgroundColor: "var(--cWhiteV2)",
+                  padding: 12,
+                  borderRadius: 8,
+                  border: "0.5px solid var(--cWhiteV1)",
+                }}
+              >
+                <p
+                  style={{
+                    color: "var(--cWhite)",
+                    fontSize: 14,
+                    fontWeight: 500,
+                  }}
+                >
+                  {day}
+                </p>
+                <p style={{ fontSize: 12, marginBottom: 8 }}>
+                  Horario disponible
+                </p>
+                <div
+                  style={{
+                    display: "flex",
+                    gap: 8,
+                    overflowX: "auto",
+                    width: "100%",
+                    scrollbarWidth: "thin",
+                    scrollbarColor: "var(--cBlackV2) var(--cWhiteV2) ",
+                  }}
+                >
+                  {item?.available_hours?.[day]?.map((hour: any) => (
+                    <div
+                      key={hour}
+                      style={{
+                        padding: 8,
+                        borderRadius: 8,
+                        border: "0.5px solid var(--cWhiteV1)",
+                        flex: "0 0 auto",
+                      }}
+                    >
+                      <p style={{ fontSize: 12 }}>{hour}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+      {openPolicy && (
+        <DataModal
+          title="Políticas"
+          open={openPolicy}
+          onClose={() => setOpenPolicy(false)}
+          buttonText=""
+          buttonCancel=""
+          className={styles.policyContainer}
+        >
+          <p className={styles.title}>Políticas de uso</p>
+          <p className={styles.subtitle}>{item?.usage_rules}</p>
+          <Br />
+          <p className={styles.title}>Políticas de reembolso</p>
+          <p className={styles.subtitle}>{item?.cancellation_policy}</p>
+        </DataModal>
       )}
-    </div>
+    </>
   );
 };
 
