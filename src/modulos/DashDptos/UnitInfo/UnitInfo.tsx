9925ba948f25aa877f49a5adfbc0850d25d62d5c
Marcelo Dávila FOS <marcelo.tester.fos@gmail.com>
2025-08-22 11:07:59 -0400

    corrected capitals for spelling

--- src/modulos/DashDptos/UnitInfo/UnitInfo.tsx
+++ src/modulos/DashDptos/UnitInfo/UnitInfo.tsx
@@ -424,7 +424,7 @@ const UnitInfo = ({
           ) : (
             <div className={styles.emptyState}>
               <EmptyData
-                message="Sin Residente asignado. Para asignar"
+                message="Sin residente asignado. Para asignar"
                 line2="un residente a esta unidad."
                 icon={<IconHomePerson2 size={32} color="var(--cWhiteV1)" />}
                 centered={true}
