781a70aad8fd7d57f9f1af554e396c76c69773bb
Esteban Hurtado - Fos <144267354+EstebanFOS@users.noreply.github.com>
2026-03-09 10:14:41 -0400

    Eliminar el todos en rango de fecha (#477)

--- src/modulos/Activities/AccessTab/AccessTab.tsx
+++ src/modulos/Activities/AccessTab/AccessTab.tsx
@@ -19,7 +19,7 @@ interface AccessesTabProps {
 }
 
 const periodOptions = [
-  { id: "ALL", name: "Todos" },
+  // { id: "ALL", name: "Todos" },
   { id: "d", name: "Hoy" },
   { id: "ld", name: "Ayer" },
   { id: "w", name: "Esta semana" },
